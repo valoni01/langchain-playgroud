f854d0a2ba3a6fd432c2e8c9a3f6201bd49ebba7
Valentine Awe <16257445+valoni01@users.noreply.github.com>
2025-09-01 11:43:11 +0200

    simple prompt template with ChatOpenAI

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from dotenv import load_dotenv
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 
 def main():
-    print("Hello from langchain!")
+    summary_template = """
+    Given the information {information} about a person, I want you to create 
+    1. A short summary
+    2. Two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template
+    )
+
+    llm = ChatOpenAI(model_name="gpt-5", temperature=0)
+
+    chain = summary_prompt_template | llm
+
+    response = chain.invoke(input={"information": "John is a software engineer from California. He loves hiking and photography. His favorite food is sushi."})
+
+    print(response.content)
 
 
 if __name__ == "__main__":
